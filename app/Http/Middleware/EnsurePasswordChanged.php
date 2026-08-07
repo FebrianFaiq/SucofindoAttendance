032fdee58df8bed51ce1b34dd7eb173f1a109eb0
AzrielEdbert <edbertkusuma@gmail.com>
2026-08-07 08:46:38 +0700

    add - ensurepasswordchanged

---
+++ app/Http/Middleware/EnsurePasswordChanged.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsurePasswordChanged
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        $exempt = $request->routeIs('force-change-password') || $request->routeIs('logout');
+
+        if ($user && $user->must_change_password && !$exempt) {
+            return redirect()->route('force-change-password');
+        }
+
+        return $next($request);
+    }
+}
